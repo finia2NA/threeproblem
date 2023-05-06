7053bce243b1dbfb5c0778f2de693c4f5fe19aac
Your Name <farther-tugboat0o@icloud.com>
2023-05-06 02:18:14 +0200

    mvp displaying points

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { OrbitControls, Point, Points, Stats } from '@react-three/drei';
+import circleImg from './assets/circle.png';
+import * as THREE from "three";
+import { useEffect, useState } from 'react';
+
+const PointCloud = props => {
+  const vertexSize = 0.05;
+  const CircleImg = useLoader(THREE.TextureLoader, circleImg);
+
+  return (
+    <Points>
+      <pointsMaterial attach={"material"}
+        map={CircleImg}
+        size={vertexSize}
+        transparent={false}
+        alphaTest={0.5}
+        opacity={1.0}
+        sizeAttenuation />
+
+      {props.positions.map((pos, i) => {
+        return <Point key={i} position={pos} />
+      })}
+    </Points >
+  )
+};
+
 
 function App() {
+
+  const [positions, setPositions] = useState([])
+
+  useEffect(() => {
+    const positions = [];
+    for (let i = 0; i < 1000; i++) {
+      const x = (Math.random() - 0.5);
+      const y = (Math.random() - 0.5);
+      const z = (Math.random() - 0.5);
+      positions.push(new THREE.Vector3(x, y, z));
+    }
+    setPositions(positions);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas camera={{ position: [0, 0, 2] }} style={{ background: "grey", width: "95vw", height: "95vh" }} >
+
+        {/* points */}
+        <PointCloud positions={positions} />
+
+        {/* controls */}
+        <OrbitControls />
+        <Stats />
+
+      </Canvas>
+    </>
   );
 }
 
